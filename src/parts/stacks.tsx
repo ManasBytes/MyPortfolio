6ff321ea705188bf66f3848f76cea8f3004128db
Manas Ranjan Sahoo <manasarmo.1819r@gmail.com>
2025-09-12 10:57:53 +0530

    GSAP added

--- src/parts/stacks.tsx
+++ src/parts/stacks.tsx
@@ -1,3 +1,30 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+import { CustomEase } from "gsap/CustomEase";
+// CustomBounce requires CustomEase
+import { CustomBounce } from "gsap/CustomBounce";
+// CustomWiggle requires CustomEase
+import { CustomWiggle } from "gsap/CustomWiggle";
+import { RoughEase, ExpoScaleEase, SlowMo } from "gsap/EasePack";
+    
+import { Draggable } from "gsap/Draggable";
+import { InertiaPlugin } from "gsap/InertiaPlugin";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { Physics2DPlugin } from "gsap/Physics2DPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+// ScrollSmoother requires ScrollTrigger
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { SplitText } from "gsap/SplitText";
+import { TextPlugin } from "gsap/TextPlugin";
+
+gsap.registerPlugin(useGSAP,Draggable,InertiaPlugin,MotionPathPlugin,MorphSVGPlugin,Physics2DPlugin,ScrollTrigger,ScrollSmoother,ScrollToPlugin,SplitText,TextPlugin,RoughEase,ExpoScaleEase,SlowMo,CustomEase,CustomBounce,CustomWiggle);
+
+
+
+
 const Stack = () => {
     return(
         <>
